a8a504a5547fbf9c387b8f60f6871f5f4712fdb0
Vasily Sliouniaev <v.sliouniaev@gmail.com>
2021-12-05 08:17:13 +0000

    20201-5

--- 2020/4/main.go
+++ 2020/4/main.go
@@ -3,40 +3,131 @@ package main
 import (
 	"fmt"
 	. "github.com/vsliouniaev/aoc/util"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Printf("Part 1: %d\n", part1("2020/3/input")) // 272
-	fmt.Printf("Part 2: %d\n", part2("2020/3/input")) // 3898725600
-
+	fmt.Printf("Part 1: %d\n", part1("2020/4/input")) // 226
+	fmt.Printf("Part 2: %d\n", part2("2020/4/input")) // 160
 }
 
 func part1(file string) int {
-	lines := ReadLinesStrings(file)
-	return countTrees(lines, 3, 1)
-}
-
-func countTrees(lines []string, right, down int) int {
-	col := 0
-	trees := 0
-	for row := down; row < len(lines); row += down {
-		line := lines[row]
-		col += right
-		if col >= len(line) {
-			col -= len(line)
+	valids := 0
+	val := validator()
+	for _, line := range ReadLinesStrings(file) {
+		if line == "" {
+			if len(val) == 0 {
+				valids++
+			}
+			val = validator()
 		}
-		if line[col] == '#' {
-			trees++
+		fields := strings.Split(line, " ")
+		for _, field := range fields {
+			kind := strings.Split(field, ":")[0]
+			delete(val, kind)
 		}
 	}
-	return trees
+	if len(val) == 0 {
+		valids++
+	}
+
+	return valids
 }
 
 func part2(file string) int {
-	lines := ReadLinesStrings(file)
-	return countTrees(lines, 1, 1) *
-		countTrees(lines, 3, 1) *
-		countTrees(lines, 5, 1) *
-		countTrees(lines, 7, 1) *
-		countTrees(lines, 1, 2)
+	valids := 0
+	passVal := validator()
+	for _, line := range ReadLinesStrings(file) {
+		if line == "" {
+			if len(passVal) == 0 {
+				valids++
+			}
+			passVal = validator()
+		}
+		fields := strings.Split(line, " ")
+		for _, field := range fields {
+			if field == "" {
+				continue
+			}
+			kv := strings.Split(field, ":")
+			var key, value = kv[0], kv[1]
+			if fun, ok := passVal[key]; ok && fun(value) {
+				delete(passVal, key)
+			}
+		}
+	}
+	if len(passVal) == 0 {
+		valids++
+	}
+
+	return valids
 }
+
+
+
+func validator() map[string]func(string) bool {
+	return map[string]func(string) bool{
+		"byr": func(s string) bool {
+			// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+			i, err := strconv.ParseInt(s, 10, 64)
+			return len(s) == 4 && err == nil && i >= 1920 && i <= 2002
+		},
+		"iyr": func(s string) bool {
+			// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+			i, err := strconv.ParseInt(s, 10, 64)
+			return len(s) == 4 && err == nil && i >= 2010 && i <= 2020
+		},
+		"eyr": func(s string) bool {
+			// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+			i, err := strconv.ParseInt(s, 10, 64)
+			return len(s) == 4 && err == nil && i >= 2020 && i <= 2030
+		},
+		"hgt": func(s string) bool {
+			//hgt (Height) - a number followed by either cm or in:
+			// If cm, the number must be at least 150 and at most 193.
+			// If in, the number must be at least 59 and at most 76.
+			h := strings.TrimSuffix(strings.TrimSuffix(s, "cm"), "in")
+			i, err := strconv.ParseInt(h, 10, 64)
+			if err != nil || len(h) == len(s) {
+				return false
+			}
+			if strings.HasSuffix(s, "cm") {
+				return i >= 150 && i <= 193
+			}
+			if strings.HasSuffix(s, "in") {
+				return i >= 59 && i <= 76
+			}
+			panic("Failed: " + s)
+		},
+		"hcl": func(s string) bool {
+			// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+			return hcl.MatchString(s)
+		},
+		"ecl": func(s string) bool {
+			// ecl (Eye Color) - exactly one of:
+			_, ok := ecl[s]
+			return ok
+		},
+		"pid": func(s string) bool {
+			// pid (Passport ID) - a nine-digit number, including leading zeroes.
+			_, err := strconv.ParseInt(s, 10, 64)
+			return len(s) == 9 && err == nil
+		},
+		//"cid": false,
+	}
+}
+
+var (
+	hcl = regexp.MustCompile("#[0-9a-f]{6}")
+	ecl = map[string]struct{} {
+		"amb": {},
+		"blu": {},
+		"brn": {},
+		"gry": {},
+		"grn": {},
+		"hzl": {},
+		"oth": {},
+	}
+)
---
+++ 2021/5/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	. "github.com/vsliouniaev/aoc/util"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Printf("Part 1: %d\n", part1("2021/5/input")) // 5145
+	fmt.Printf("Part 2: %d\n", part2("2021/5/input")) // 16518
+}
+
+func part1(file string) int {
+	pos := make(map[string]int)
+	for _, line := range ReadLinesStrings(file) {
+		seg := parse(line)
+		var s int
+		var e int
+		var f string
+		if seg.isHorz() {
+			f = "%d," + strconv.Itoa(seg.sy)
+			s = seg.sx
+			e = seg.ex
+		} else if seg.isVert() {
+			f = strconv.Itoa(seg.sx) + ",%d"
+			s = seg.sy
+			e = seg.ey
+		} else {
+			continue
+		}
+		if e < s {
+			s, e = e, s
+		}
+		for ; s <= e; s++ {
+			p := fmt.Sprintf(f, s)
+			count := pos[p]
+			pos[p] = count + 1
+		}
+	}
+	count := 0
+	for _, v := range pos {
+		if v > 1 {
+			count++
+		}
+	}
+	return count
+}
+
+func part2(file string) int {
+	pos := make(map[string]int)
+	for _, line := range ReadLinesStrings(file) {
+		seg := parse(line)
+		for {
+			x, y, zero := seg.shrink()
+			p := fmt.Sprintf("%d,%d", x, y)
+			count := pos[p]
+			pos[p] = count + 1
+			if zero {
+				break
+			}
+		}
+	}
+	count := 0
+	for _, v := range pos {
+		if v > 1 {
+			count++
+		}
+	}
+	return count
+}
+
+func parse(line string) (s segment) {
+	var err error
+	se := strings.Split(line, " -> ")
+	start := strings.Split(se[0], ",")
+	end := strings.Split(se[1], ",")
+
+	s.sx, err = strconv.Atoi(start[0])
+	Check(err)
+	s.sy, err = strconv.Atoi(start[1])
+	Check(err)
+
+	s.ex, err = strconv.Atoi(end[0])
+	Check(err)
+	s.ey, err = strconv.Atoi(end[1])
+	Check(err)
+	return
+}
+
+type segment struct {
+	sx int
+	ex int
+	sy int
+	ey   int
+}
+
+func (s *segment) isHorzVert() bool {
+	return s.isHorz() || s.isVert()
+}
+
+func (s *segment) isHorz() bool {
+	return s.sy == s.ey
+}
+
+func (s *segment) isVert() bool {
+	return s.sx == s.ex
+}
+
+// shrink moves the start point of the segment towards the end by 1 step. It returns the old (un-shrunk) x,y coordinates
+// of the start point and whether the length of the remaining segment is zero - i.e. if it makes sense to shrink the
+// segment again.
+func (s *segment) shrink() (x, y int, zero bool) {
+	zero = true
+	x = s.sx
+	y = s.sy
+	dx := s.ex - s.sx
+	dy := s.ey - s.sy
+	if dx > 0 {
+		zero = false
+		s.sx++
+	} else if dx < 0 {
+		zero = false
+		s.sx--
+	}
+	if dy > 0 {
+		zero = false
+		s.sy++
+	} else if dy < 0 {
+		zero = false
+		s.sy--
+	}
+	return
+}
