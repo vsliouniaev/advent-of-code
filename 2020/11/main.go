4dd054f97d1539c133dcef666b4973b61dc81a26
Vasily Sliouniaev <v.sliouniaev@gmail.com>
2021-12-09 19:49:09 +0000

    2020-11

--- 2020/11/main.go
+++ 2020/11/main.go
@@ -6,52 +6,52 @@ import (
 )
 
 func main() {
-	fmt.Printf("Part 1: %d\n", part1("2020/11/input"))
-	//fmt.Printf("Part 2: %d\n", part2("2020/11/input"))
+	fmt.Printf("Part 1: %d\n", part1("2020/11/input")) // 2178
+	fmt.Printf("Part 2: %d\n", part2("2020/11/input")) // 1978
 }
 
 const (
 	Empty    = 'L'
 	Occupied = '#'
-	Blocked  = '.'
+	Floor    = '.'
 )
 
 func part1(file string) int {
-	nextLines := util.ReadLinesRunes(file)
+	nextLines := grid(util.ReadLinesRunes(file))
 	maxy := len(nextLines) - 1
 	maxx := len(nextLines[0]) - 1
 
-	i := 0
 	for {
 		lines := nextLines
 		nextLines = makeNextGrid(lines)
 		changed := 0
 		for y := 0; y <= maxy; y++ {
 			for x := 0; x <= maxx; x++ {
-				cell := lines[y][x]
-				if cell == Blocked {
+				p := point{x: x, y: y}
+				cell := lines.val(p)
+				if cell == Floor {
 					continue
 				}
-				neighbours := point{x: x, y: y}.neighbours(maxx, maxy)
+				neighbours := p.neighbours(nextLines)
 				occ := 0
 				for _, n := range neighbours {
-					if lines[n.y][n.x] == Occupied {
+					if lines.val(n) == Occupied {
 						occ++
 					}
 				}
 
-				if occ >= 4 && lines[y][x] == Occupied {
-					nextLines[y][x] = Empty
+				if occ >= 4 && cell == Occupied {
+					nextLines.set(p, Empty)
 					changed++
 				}
 
-				if occ == 0 && lines[y][x] == Empty {
-					nextLines[y][x] = Occupied
+				if occ == 0 && cell == Empty {
+					nextLines.set(p, Occupied)
 					changed++
 				}
 			}
 		}
-		i++
+
 		if changed == 0 {
 			break
 		}
@@ -72,7 +72,7 @@ func countOccupied(lines [][]rune) int {
 	return cnt
 }
 
-func makeNextGrid(in [][]rune) [][]rune {
+func makeNextGrid(in [][]rune) grid {
 	out := make([][]rune, len(in))
 	for y, row := range in {
 		out[y] = make([]rune, len(row))
@@ -89,7 +89,9 @@ type point struct {
 	y int
 }
 
-func (p point) neighbours(maxx, maxy int) []point {
+func (p point) neighbours(g grid) []point {
+	maxy := len(g) - 1
+	maxx := len(g[0]) - 1
 	all := []point{
 		{x: p.x + 1, y: p.y - 0}, // e
 		{x: p.x + 1, y: p.y - 1}, // se
@@ -112,6 +114,80 @@ func (p point) neighbours(maxx, maxy int) []point {
 	return pts
 }
 
+func occupied(p point, g grid) int {
+	maxy := len(g) - 1
+	maxx := len(g[0]) - 1
+	getResult := func(addx, addy int) int {
+		x := p.x + addx
+		y := p.y + addy
+		for x >= 0 && y >= 0 && x <= maxx && y <= maxy {
+			switch g[y][x] {
+			case Floor:
+				x += addx
+				y += addy
+			case Occupied:
+				return 1
+			case Empty:
+				return 0
+			}
+		}
+		return 0
+	}
+
+	return getResult(+1, +0) + // e
+		getResult(+1, -1) + // se
+		getResult(+0, -1) + // s
+		getResult(-1, -1) + // sw
+		getResult(-1, +0) + // w
+		getResult(-1, +1) + // nw
+		getResult(-0, +1) + // n
+		getResult(+1, +1) // ne
+
+}
+
+type grid [][]rune
+
+func (g grid) val(p point) rune {
+	return g[p.y][p.x]
+}
+
+func (g grid) set(p point, r rune) {
+	g[p.y][p.x] = r
+}
+
 func part2(file string) int {
-	return 0
+	nextLines := grid(util.ReadLinesRunes(file))
+	maxy := len(nextLines) - 1
+	maxx := len(nextLines[0]) - 1
+
+	for {
+		lines := nextLines
+		nextLines = makeNextGrid(lines)
+		changed := 0
+		for y := 0; y <= maxy; y++ {
+			for x := 0; x <= maxx; x++ {
+				p := point{x: x, y: y}
+				cell := lines.val(p)
+				if cell == Floor {
+					continue
+				}
+
+				occ := occupied(p, lines)
+				if occ >= 5 && cell == Occupied {
+					nextLines.set(p, Empty)
+					changed++
+				}
+
+				if occ == 0 && cell == Empty {
+					nextLines.set(p, Occupied)
+					changed++
+				}
+			}
+		}
+		if changed == 0 {
+			break
+		}
+	}
+
+	return countOccupied(nextLines)
 }
